4eccfa8221d79732197b3d3416018d8511811f0b
Tarcísio Vitor de Souza <tarcisiovity@hotmail.com>
2018-11-24 17:27:08 -0200

    DbContext

---
+++ EasySoccer.WebApi/EasySoccer.DAL.Infra/IEasySoccerDbContext.cs
@@ -0,0 +1,24 @@
+﻿using EasySoccer.Entities;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasySoccer.DAL.Infra
+{
+    public interface IEasySoccerDbContext
+    {
+        #region Infra
+        Task<int> SaveChangesAsync();
+        Task Add<TEntity>(TEntity entity) where TEntity : class;
+        Task Edit<TEntity>(TEntity entity) where TEntity : class;
+        Task Delete<TEntity>(TEntity entity) where TEntity : class;
+        #endregion
+
+        IQueryable<User> UserQuery { get; }
+        IQueryable<Company> CompanyQuery { get; }
+        IQueryable<CompanySchedule> CompanyScheduleQuery { get; }
+        IQueryable<SoccerPitch> SoccerPitchQuery { get; }
+        IQueryable<SoccerPitchPictures> SoccerPitchPicturesQuery { get; }
+        IQueryable<SoccerPitchPlan> SoccerPitchPlanQuery { get; }
+        IQueryable<SoccerPitchReservation> SoccerPitchReservationQuery { get; }
+    }
+}
---
+++ EasySoccer.WebApi/EasySoccer.DAL/EasySoccerDbContext.cs
@@ -0,0 +1,63 @@
+﻿using EasySoccer.DAL.Infra;
+using EasySoccer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasySoccer.DAL
+{
+    public class EasySoccerDbContext : DbContext, IEasySoccerDbContext
+    {
+        public DbSet<User> User { get; set; }
+        public IQueryable<User> UserQuery { get { return User; } }
+
+        public DbSet<Company> Company { get; set; }
+        public IQueryable<Company> CompanyQuery { get { return Company; } }
+
+        public DbSet<CompanySchedule> CompanySchedule { get; set; }
+        public IQueryable<CompanySchedule> CompanyScheduleQuery { get { return CompanySchedule; } }
+
+        public DbSet<SoccerPitch> SoccerPitch { get; set; }
+        public IQueryable<SoccerPitch> SoccerPitchQuery { get { return SoccerPitch; } }
+
+        public DbSet<SoccerPitchPictures> SoccerPitchPictures { get; set; }
+        public IQueryable<SoccerPitchPictures> SoccerPitchPicturesQuery { get { return SoccerPitchPictures; } }
+
+        public DbSet<SoccerPitchPlan> SoccerPitchPlan { get; set; }
+        public IQueryable<SoccerPitchPlan> SoccerPitchPlanQuery { get { return SoccerPitchPlan; } }
+
+        public DbSet<SoccerPitchReservation> SoccerPitchReservation { get; set; }
+        public IQueryable<SoccerPitchReservation> SoccerPitchReservationQuery { get { return SoccerPitchReservation; } }
+
+        #region Infra
+        public Task Delete<TEntity>(TEntity entity) where TEntity : class
+        {
+            return Task.Run(() =>
+            {
+                base.Remove(entity);
+            });
+        }
+
+        public Task Edit<TEntity>(TEntity entity) where TEntity : class
+        {
+            return Task.Run(() =>
+            {
+                base.Update(entity);
+            });
+        }
+
+        public Task<int> SaveChangesAsync()
+        {
+            return base.SaveChangesAsync();
+        }
+
+        public Task Add<TEntity>(TEntity entity) where TEntity : class
+        {
+            return Task.Run(() =>
+            {
+                base.Add(entity);
+            });
+        } 
+        #endregion
+    }
+}
