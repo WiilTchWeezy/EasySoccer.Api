2bf2093b5ad5c0f6c21f0d98887ce1e3b23428c1
Tarcísio Vitor de Souza <tarcisiovity@hotmail.com>
2021-06-26 16:14:34 -0300

    WathsAppRequest

--- EasySoccer.WebApi/EasySoccer.WebApi/ApiRequests/WathsappRequest.cs
+++ EasySoccer.WebApi/EasySoccer.WebApi/ApiRequests/WathsappRequest.cs
@@ -4,12 +4,5 @@
     {
         public string conversationContactId { get; set; }
         public string incomingMessage { get; set; }
-        public string channelId﻿﻿ { get; set; }
-        public string receivedConversationMessage﻿﻿ { get; set; }
-        public string conversationMessageType { get; set; }
-        public string conversationMessageContent { get; set; }
-        public string message { get; set; }
-        public string currentTime﻿﻿ { get; set; }
-        public string invocationId { get; set; }
     }
 }
